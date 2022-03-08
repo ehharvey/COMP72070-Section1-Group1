1ac6ca18f7c9b8e7a0fea4eb2fafe404db4515ad
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-03-08 17:21:19 -0500

    Rough scratch #1

--- Client/Client.h
+++ Client/Client.h
@@ -5,4 +5,14 @@ namespace Client {
 	const int EXPECTED_FOO = 3;
 
 	int getFOO();
+
+	class Client {
+	private:
+		/*
+		* Variables:
+		* - IP address
+		* - ClientRequest 
+		*/
+		Communication::ClientRequest ClientRequest;
+	};
 }
--- Communication/Communication.h
+++ Communication/Communication.h
@@ -1,10 +1,58 @@
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 namespace Communication {
 	const int HELLO = 1;
 
-	struct ClientRequest {
-		unsigned char AuthByte;
-		unsigned char CommandByte;
+	class ClientRequest {
+	private:
+		struct data {
+			uint8_t AuthByte;
+			uint8_t CommandByte;
+		} Data;
+	public:
+
+	};
+
+	struct ServerResponse {
+	private:
+		struct data {
+			uint8_t ResultByte;
+			struct status {
+				uint8_t Payload[2];
+			} Status;
+		} Data;
+	public:
+
+	};
+
+	typedef struct ipv4_address {
+		uint8_t octet[3]; 
+	} IPV4Address;
+
+	__interface ITCPNetworker
+	{
+		void Initialize();
+		void ConnectTo();
+		void Send();
+		void Receive();
+		void Close();
+	};
+
+	class IPSocket : ITCPNetworker
+	{
+		IPV4Address local;
+		std::vector<IPV4Address> remotes; // We can have 0 or more remotes
+
+	public:
+		IPSocket(IPV4Address local);
+
+		void Initialize();
+		void ConnectTo();
+		void Send();
+		void Receive();
+		void Close();
 	};
 }
