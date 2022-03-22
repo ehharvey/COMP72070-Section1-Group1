4c5644fa06714c35173232e30af6b0350b2ba10b
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-03-22 09:15:26 -0400

    Unit Tests

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -8,7 +8,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
 {
-	TEST_CLASS(CommunicationTests)
+	TEST_CLASS(Data_TESTS)
 	{
 	public:
 			
@@ -23,7 +23,46 @@ namespace UnitTests
 			Communication::IData& myData = Communication::Data((uint8_t*)payload.c_str(), size);
 			
 			// Assert
-			// shouldn't crash
+			Assert::AreEqual(EXPECTED_SIZE, myData.getSize());
+		}
+
+		TEST_METHOD(getPayloadCheck) {
+			// Arrange
+			std::unique_ptr<const uint8_t> s(new const uint8_t[]{ "Hello World" });
+			const size_t SIZE = strlen("Hello World") + 1;
+
+
+			// Act
+			Communication::Data myData = Communication::Data(std::move(s), SIZE);
+
+			// Assert
+			bool payload_is_the_same = strcmp("Hello World", (const char*)(myData.getPayload().get())) == 0;
+			Assert::IsTrue(payload_is_the_same);
+		}
+	};
+
+	TEST_CLASS(ClientRequest_TESTS) 
+	{
+	private:
+	public:
+		TEST_METHOD(Constructor) {
+			// Arrange
+
+			// Act
+
+			// Assert
+			Communication::ClientRequest actual; // Shouldn't crash
+		}
+
+		TEST_METHOD(INCOMPLETE_ConstructorDeserialization) {
+			// Arrange
+			const Communication::ClientRequest EXPECTED;
+
+			// Act
+			Communication::ClientRequest actual(std::unique_ptr<Communication::IData>(new Communication::Data()));
+
+			// Assert
+			
 		}
 	};
 }
