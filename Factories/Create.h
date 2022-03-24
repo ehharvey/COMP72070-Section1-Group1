171f484b25f268c24ced04c314f32fde5d5e52c9
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-03-24 09:12:48 -0400

    Removed Factories project

--- Factories/Create.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../Client/Client.h"
-#include "../Communication/Communication.h"
-#include "../Logger/Logger.h"
-#include "../Server/Server.h"
-#include "../Tamagotchi/Tamagotchi.h"
-#include <memory>
