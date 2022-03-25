b103512e0068285c2fae9344844f107cb1b1d47a
ColinSmitty <43004159+ColinSmitty@users.noreply.github.com>
2022-03-25 13:42:13 -0400

    Added more 'structure' to CLI

--- ClientCLI/ClientCLI.cpp
+++ ClientCLI/ClientCLI.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "CLI.h"
 #include "..\Client\Client.h"
 #include "..\Communication\Communication.h"
 #include "..\Logger\Logger.h"
@@ -10,19 +11,61 @@
 
 int main()
 {
-    enum inputOptions {help, feed, clean, sleep, quit, invalid};
-    
-    bool continueCLI = true;
+    cout << "Tamagotchi [Version 1.0] \n";
+    cout << "COMP72070. Group #1\n\n";
 
-    do
+    bool Quit = false;
+
+    string userInput;
+
+    while (!Quit)
     {
-        //show menu
-        //get input
-            //update bool if necessary
-        //parse digits if any
-        //send command to server
-        //show response
+        hashit();
+        cout << "\Tamagotchi> ";
+        cin >> userInput;
 
-    } while (continueCLI);
+        string command;
+        string amount;
+
+        for (int i = 0; i < userInput.length(); i++)
+        {
+            if (userInput[i] == ' ')
+            {
+                command.append(userInput.substr(0, i));
+                amount.append(userInput.substr(i + 1, userInput.length() - 1));
+
+                break;
+            }
+        }
+
+        switch (string_commands[userInput])
+        {
+        case eFeed:
+        {
+            break;
+        }
+
+        case eSleep:
+        {
+            break;
+        }
+        case eClean:
+        {
+            break;
+        }
+        case eHelp:
+        {
+            help();
+            break;
+        }
+        case QUIT:
+        {
+            cout << "Goodbye!";
+
+            Quit = true;
+            break;
+        }
+        }
+    }
     return 0;
 }
