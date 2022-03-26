c02fd1e6e110e492b4a5e766ca28fb6f704010ff
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-03-26 10:39:11 -0400

    More in-line documentation and example

--- Communication/Examples/Examples.cpp
+++ Communication/Examples/Examples.cpp
@@ -2,6 +2,21 @@
 #include "../Create.h";
 #include <iostream>
 
+// Declared const so that myFunc does not get redefined
+const Communicators::rPtr myFunc = [](std::vector<uint8_t> request)
+{
+	// Retrieves an integer
+	int request_integer;
+	std::memcpy((void*) &request_integer, (void*) request.front(), sizeof(int));
+
+	request_integer++;
+
+	std::vector<uint8_t> result(sizeof(int)); // Initializes the vector with ~4 bytes
+	std::memcpy((void*) result.front(), &request_integer, sizeof(int));
+
+	return result;
+};
+
 void __TcpHost_Example()
 {
 	Data::IPV4Address test = { 192, 168, 100, 1 };
