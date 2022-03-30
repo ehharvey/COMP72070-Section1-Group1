cba0700e7435b7a801938528b05bbd337bf19f02
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-03-29 21:29:52 -0400

    Removed fake test

--- Testing/Unit/CommunicationTests.cpp
+++ Testing/Unit/CommunicationTests.cpp
@@ -2,15 +2,6 @@
 #include "../Mocks/Mocks.h"
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-
-  auto client = Create::ClientRequest();
-}
 
 TEST(ClientRequestTests, Constructor)
 {
